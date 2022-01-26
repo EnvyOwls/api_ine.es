5f99da1fd195f799f2d38b0e26fa4daa5b1e02e5
EnvyOwls <48996547+EnvyOwls@users.noreply.github.com>
2022-01-26 00:47:26 -0300

    Create testApi.dart

---
+++ testApi.dart
@@ -0,0 +1,148 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget{
+
+  _estadisticas createState() => _estadisticas();
+}
+
+class _estadisticas extends State<MyApp>{
+
+  Future<List<Data>>? _dataINE;
+
+  Future<List<Data>> _getData() async{
+
+    List<Data>? datos = [];
+
+    final url = Uri.parse('https://servicios.ine.es/wstempus/js/ES/VALORES_VARIABLE/706?page=1');
+    final response = await http.get(url);
+
+    if(response.statusCode == 200){
+      String data = utf8.decode(response.bodyBytes);
+      final jsonData = json.decode(data);//jsonDecode(data);
+
+      //int i = 0;
+      for(var migrante in jsonData){
+        datos.add(
+            Data(migrante['Nombre'])
+        );
+        // print(migrante['Nombre']);i++;
+      }
+    }
+    else{
+      print("hay error :(!");
+    }
+
+    return datos;
+  }
+
+  void initState(){
+    super.initState();
+    _dataINE = _getData();
+  }
+
+  var size;
+  Widget build(BuildContext context){
+
+
+    return MaterialApp(
+        title: 'test API ine.es',
+
+        home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text('Datos INE España', style: TextStyle(color: Colors.black12),),
+            backgroundColor: Color.fromARGB(200, 153, 234, 218),
+          ),
+
+          body: FutureBuilder(
+            future: _dataINE,
+            builder: (context, snapshot) {
+              size = MediaQuery.of(context).size.width;
+              if(snapshot.hasData){
+                //print(snapshot.data);
+                return Container(
+                  //widthFactor: size*0.5,
+                  //width: size*0.5,
+                  //alignment: Alignment.topRight,
+                  child: Table(
+                    children: _listNacionalidad(snapshot.data),
+                    border: TableBorder.all(color: Colors.indigo),
+                    defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                  ),
+                );
+              }
+              return Center(
+                child: CircularProgressIndicator(color: Colors.teal,),
+              );
+            },
+          ),
+          //Table(
+          //children: [TableRow(children: [Text("Nacionalidad"), Text('Cantidad')])],
+          //border: TableBorder.all(color: Colors.blueAccent),
+        )
+      //),
+    );
+  }
+
+  List<TableRow> _listNacionalidad(data){
+
+    List<TableRow> nacionalidad = [];
+    List<Data2> datos = procesar(data);
+    for(var pais in datos){
+      nacionalidad.add(
+          TableRow(children: [Text(pais.nacionalidad, textAlign: TextAlign.center,), Text('${pais.cantidad}')])
+      );
+    }
+
+
+    return nacionalidad;
+  }
+
+  List<Data2> procesar(List<Data> data){
+
+    List<Data2> retorno = [];
+    List<Data> temp = data;
+
+    var first;
+    int total = 0;
+    for(var item in temp){
+      first = item.nacionalidad;
+      total = count(first, temp);
+      retorno.add(Data2(first, total));
+      //print('${first},${total}');
+      temp.removeWhere((item) => item.nacionalidad == first);
+    }
+    return retorno;
+  }
+
+  int count(String dato, List<Data> datos){
+    int cantidad = 0;
+    for (var i in datos){
+      if(dato == i.nacionalidad){
+        cantidad++;
+      }
+    }
+
+    return cantidad;
+  }
+}
+
+class Data{
+  String nacionalidad;
+
+  Data(this.nacionalidad);
+}
+
+class Data2{
+  String nacionalidad;
+  int cantidad;
+
+  Data2(this.nacionalidad,this.cantidad);
+}
